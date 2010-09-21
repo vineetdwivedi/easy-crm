6dc62347a510bf0e9afe2149bcfb89e3a1bd329c
kondotine <kondotine@db4f0a9b-84e0-4251-ba91-1d70aa74380d>
2010-09-21 20:12:18 +0000

    -Starting the admin part. -IMembershipService.cs and MembershipService.cs moved to EasyCRM.Model.Services and EasyCRM.Model.Services.Impl;

---
+++ trunk/easy-crm/EasyCRM.Admin/EasyCRMForm.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows.Forms;
+using EasyCRM.Model.Services;
+using EasyCRM.Model.Services.Impl;
+using System.Collections.Generic;
+
+namespace EasyCRM.Admin
+{
+    public partial class EasyCRMForm : Form
+    {   
+        //services
+        private IMembershipService _membershipService;
+        private IUserService _userService;
+        private ITaskService _taskService;
+        private IContactService _contactService;
+        private IAccountService _accountService;
+        private IOpportunityService _opportunityService;
+        private IIndustrialSectorService _sectorService;
+
+        //
+        private Dictionary<string, string> _userModelState = new Dictionary<string, string>();
+        private Dictionary<string, string> _taskModelState = new Dictionary<string, string>();
+        private Dictionary<string, string> _sectorModelState = new Dictionary<string, string>();
+
+        public EasyCRMForm()
+        {
+            InitializeComponent();
+            InitializeServices();            
+
+        }
+        
+        private void InitializeServices()
+        {
+            _userService = new UserService(new SimpleModelStateWrapper(_userModelState));
+            _taskService = new TaskService(new SimpleModelStateWrapper(_taskModelState));
+            _sectorService = new IndustrialSectorService(new SimpleModelStateWrapper(_taskModelState));
+        }
+
+        private void EasyCRMForm_Load(object sender, System.EventArgs e)
+        {
+            setUsers();
+            setSectors();
+        }
+
+
+        private void setUsers()
+        {
+            this.userBindingSource.DataSource = _userService.ListUsers(); 
+        }
+
+        private void setSectors()
+        {
+            this.sectorBindingSource.DataSource = _sectorService.ListIndustrialSectors(); 
+        }
+
+
+
+    }
+}
